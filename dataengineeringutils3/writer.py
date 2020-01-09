3bd2de0a80cef072a38e203e3bf61897bd0b2252
Karik Isichei <karik.isichei@gmail.com>
2020-01-09 16:17:09 +0000

    Large rewrite of writer module and subsequent tests. Writer classes now work as file like objects so can just be passed to basic writers like csv or jsonlines packages. See UTs. Have created two subclasses from the base class Bytes and String to mimic io.

--- dataengineeringutils3/writer.py
+++ dataengineeringutils3/writer.py
@@ -1,90 +1,205 @@
 import sys
+import os
+import gzip
+import boto3
 
-from dataengineeringutils3.s3 import gzip_string_write_to_s3
+from dataengineeringutils3.s3 import (
+    gzip_string_write_to_s3,
+    s3_path_to_bucket_key,
+)
 
+from io import BytesIO, StringIO
 
-class SplitFileWriter:
+class BaseSplitFileWriter:
     """
-    Base class for splitting large datasets in to chunks and writing to s3
+    Base class for splitting large datasets in to chunks and writing to s3.
+    This is acts as a file like object. Data is written to an in memory file
+    (note this file is defined in subclasses and not set in this base class).
+    until it hits a max_bytes limit at which point the data is written to S3
+    as single file. The in memory file is defined by the sub classes 
+    BytesSlitFileWriter and StringSplitFileWriter. These subclasses attempt
+    to mimic the expected response of BytesIO and StringIO. 
 
-    The extension and the _write methods are defined in classes which extend this class
+    :param s3_basepath: The base path to the s3 location you want to write to S3://...
+    :param filename_prefix: The filename that you want to keep constant. Every written file is prefixed with this string.
+    S3 objects written will end in the file number and the extension.
+    :param max_bytes: The maximum number of bytes for each file (uncompressed file size) default set at 1GB.
+    :param compress_on_upload: If the file should be compressed before writing to S3 (default True). Note does not affect 
+    the file_extension parameter.
+    :param file_extension: String representing the file extension. Should not be prefixed with a '.'.
 
-    lines = [
-        '{"key": "value"}'
-    ]
-    with JsonNlSplitFileWriter("s3://test/test-file.josnl.gz") as writer:
-        for line in lines:
-            writer.write_line(line)
+    :Example:
+
+    from dataengineeringutils3.writer import BytesSplitFileWriter
+    from jsonlines import Writer
+
+    test_dict = {"foo": "bar"}
+    
+    bsfw = BytesSplitFileWriter("s3://test/folder/", "test-file", max_bytes=30, compress_on_upload=False, file_extension="jsonl")
+
+    # Same functionality passing 
+    json_writer = Writer(bsfw)
+    for i in range(0, 10):
+        json_writer.write(test_dict)
+    
+    json_writer.close()
+
+    # Closing the objects sends off the remaining data in the io buffer
+    bsfw.close()
     """
 
-    extension = ""
-
-    def __init__(self, outfile_path, max_bytes=800000000, chunk_size=1000):
-        """
-        Set the outfile path and specify the chunk byte size and line number chunk size
-
-        :param outfile_path: string: s3://test/test-file.jsonl.gz
-        :param max_bytes: int: Bytes to chunk file to: 800000000
-        :param chunk_size: int: number of rows  check file size for chunking: 1000
-        """
-        self.outfile_path = outfile_path.replace(self.extension, "")
+    def __init__(self, s3_basepath, filename_prefix, max_bytes=1000000000, compress_on_upload=True, file_extension=None):
+        self.filename_prefix = filename_prefix
+        self.s3_basepath = s3_basepath
         self.max_bytes = max_bytes
-        self.chunk_size = chunk_size
-        self.string = ""
-        self.total_lines = 0
-        self.num_lines = 0
         self.num_files = 0
+        self.mem_file = None
+        self.compress_on_upload = compress_on_upload
+        self.file_extension = "" if file_extension is None else file_extension
+        self.mem_file = self.get_new_mem_file()
 
     def __enter__(self):
-        self.string = ""
-        self.num_lines = 0
+        self.mem_file = self.get_new_mem_file()
         self.num_files = 0
         return self
 
     def __exit__(self, *args):
         self.close()
 
-    def write_line(self, line):
-        """Writes line as string"""
-        self.string += f"{line}\n"
-        self.num_lines += 1
-        if (
-            not self.num_lines % self.chunk_size
-            and sys.getsizeof(self.string) > self.max_bytes
-        ):
-            self.write_file()
+    def get_new_mem_file(self):
+        """
+        Overwritten by subclasses. Should return a filelike object.
+        """
+        return ""
 
-    def write_all_lines(self, lines, line_transform=lambda x: x):
-        [self.write_line(line_transform(line)) for line in lines]
+    def _compress_data(self, data):
+        """
+        Can be overwritten by subclasses. Should return compressed data.
+        """
+        return gzip.compress(data)
 
-    def write_lines(self, lines, line_transform=lambda x: x):
-        self.string += "\n".join(line_transform(l) for l in lines) + "\n"
-        self.num_lines += len(lines)
-        if sys.getsizeof(self.string) > self.max_bytes:
-            self.write_file()
+    def write(self, b):
+        self.mem_file.write(b)
+        if self.file_size_limit_reached():
+            self.write_to_s3()
 
-    def _write(self, file_path):
-        """Writes file part to local storage"""
-        with open(file_path, "+wb") as f:
-            f.write(bytes(self.string, "utf-8"))
 
-    def write_file(self):
-        """Writes and updates number of files and sets lines to 0"""
-        self._write(f"{self.outfile_path}_{self.num_files}{self.extension}")
+    def writelines(self, lines):
+        self.mem_file.writelines(lines)
+        if self.file_size_limit_reached():
+            self.write_to_s3()
+
+
+    def file_size_limit_reached(self):
+        if (self.max_bytes) and (self.mem_file.tell() > self.max_bytes):
+            return True
+        else:
+            return False
+
+    def write_to_s3(self):
+        s3_resource = boto3.resource("s3")
+        b, k = s3_path_to_bucket_key(self.get_s3_filepath())
+        data = self.mem_file.getvalue()
+        if self.compress_on_upload:
+            data = self._compress_data(data)
+        
+        s3_resource.Object(b, k).put(Body=data)
+        
+        self.reset_file_buffer()
+
+    def reset_file_buffer(self):
         self.num_files += 1
-        self.string = ""
-        self.total_lines += self.num_lines
-        self.num_lines = 0
+        self.mem_file.close()
+        self.mem_file = self.get_new_mem_file()
+
+    def get_s3_filepath(self):
+        fn = f"{self.filename_prefix}-{self.num_files}.{self.file_extension}"
+        return os.path.join(self.s3_basepath, fn)
 
     def close(self):
         """Write all remaining lines to a final file"""
-        if self.num_lines:
-            self.write_file()
+        if self.mem_file.tell():
+            self.write_to_s3()
+            self.mem_file.close()
 
 
-class JsonNlSplitFileWriter(SplitFileWriter):
-    extension = ".jsonl.gz"
+class BytesSplitFileWriter(BaseSplitFileWriter):
+    """
+    BytesIO file like object for splitting large datasets in to chunks and
+    writing to s3. Data is written to a BytesIO file buffer until it hits a
+    max_bytes limit at which point the data is written to S3
+    as single file.
 
-    def _write(self, file_path):
-        """Gzip string to file path in s3"""
-        gzip_string_write_to_s3(self.string, file_path)
+    :param s3_basepath: The base path to the s3 location you want to write to S3://...
+    :param filename_prefix: The filename that you want to keep constant. Every written file is prefixed with this string.
+    S3 objects written will end in the file number and the extension.
+    :param max_bytes: The maximum number of bytes for each file (uncompressed file size) default set at 1GB.
+    :param compress_on_upload: If the file should be compressed before writing to S3 (default True). Note does not affect 
+    the file_extension parameter.
+    :param file_extension: String representing the file extension. Should not be prefixed with a '.'.
+
+    :Example:
+
+    from dataengineeringutils3.writer import BytesSplitFileWriter
+    from jsonlines import Writer
+
+    test_dict = {"foo": "bar"}
+    
+    bsfw = BytesSplitFileWriter("s3://test/folder/", "test-file", max_bytes=30, compress_on_upload=False, file_extension="jsonl")
+
+    # Same functionality passing 
+    json_writer = Writer(bsfw)
+    for i in range(0, 10):
+        json_writer.write(test_dict)
+    
+    json_writer.close()
+
+    # Closing the objects sends off the remaining data in the io buffer
+    bsfw.close()
+    """
+    def get_new_mem_file(self):
+        return BytesIO()
+
+
+class StringSplitFileWriter(BaseSplitFileWriter):
+    """
+    StringIO file like object for splitting large datasets in to chunks and
+    writing to s3. Data is written to a StringIO file buffer until it hits a
+    max_bytes limit at which point the data is written to S3
+    as single file.
+
+    :param s3_basepath: The base path to the s3 location you want to write to S3://...
+    :param filename_prefix: The filename that you want to keep constant. Every written file is prefixed with this string.
+    S3 objects written will end in the file number and the extension.
+    :param max_bytes: The maximum number of bytes for each file (uncompressed file size) default set at 1GB.
+    :param compress_on_upload: If the file should be compressed before writing to S3 (default True). Note does not affect 
+    the file_extension parameter.
+    :param file_extension: String representing the file extension. Should not be prefixed with a '.'.
+
+    :Example:
+
+    from dataengineeringutils3.writer import StringSplitFileWriter
+    from jsonlines import Writer
+
+    test_dict = {"foo": "bar"}
+    
+    ssfw = StringSplitFileWriter("s3://test/folder/", "test-file", max_bytes=30, compress_on_upload=False, file_extension="jsonl")
+
+    # Same functionality passing 
+    json_writer = Writer(ssfw)
+    for i in range(0, 10):
+        json_writer.write(test_dict)
+    
+    json_writer.close()
+
+    # Closing the objects sends off the remaining data in the io buffer
+    ssfw.close()
+    """
+    def get_new_mem_file(self):
+        return StringIO()
+
+    def _compress_data(self, data):
+        """
+        Converts string data to bytes and then compresses
+        """
+        return gzip.compress(bytes(data, "utf-8"))
--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1,68 +1,179 @@
 import sys
+import gzip
 
 import pytest
+import csv
+from io import StringIO, BytesIO
 
 from dataengineeringutils3.s3 import gzip_string_write_to_s3
-from dataengineeringutils3.writer import JsonNlSplitFileWriter
+from dataengineeringutils3.writer import (
+    BytesSplitFileWriter,
+    StringSplitFileWriter
+)
 from tests.helpers import time_func
 
+import jsonlines
 
-def test_json_split_file_writer(s3):
-    """Test Writer splits files, gzips and sends to s3"""
-    file_key = "test-key"
+@pytest.mark.parametrize(
+    "folder,filename,compress",
+    [
+        ("test-csv/", "test-file", False),
+        ("", "a", True),
+    ],
+)
+def test_with_csv_string_split_file_writer(s3, folder, filename, compress):
+    """Test string writer with statement csv"""
     bucket_name = "test"
-    s3_path = f"s3://{bucket_name}/{file_key}"
     s3.meta.client.create_bucket(Bucket=bucket_name)
-    bucket = s3.Bucket(bucket_name)
-    with JsonNlSplitFileWriter(s3_path, 1024, 2) as writer:
-        for i in range(150):
-            writer.write_line(f"{i}. This test line number {i + 1}")
-    assert writer.total_lines == 150
-    keys_in_bucket = [f"s3://{bucket_name}/{o.key}" for o in bucket.objects.all()]
-    files_in_bucket = len(keys_in_bucket)
-    assert files_in_bucket == 5
-    assert keys_in_bucket == [f"{s3_path}_{i}.jsonl.gz" for i in range(files_in_bucket)]
+
+    csv_data = [("i","x1","x2"), (1,"a","b"), (2,"a","b"), (3,"a","b"),(4,"a","b"),(5,"a","b"),(6,"a","b"),(7,"a","b")]
+
+    expected_file = StringIO()
+    e_csv_writer = csv.writer(expected_file)
+
+    ext = "csv.gz" if compress else "csv"
+    
+    # Test using with statement
+    with StringSplitFileWriter(f"s3://{bucket_name}/{folder}", filename, max_bytes=30, compress_on_upload=compress, file_extension=ext) as f:
+        csv_writer = csv.writer(f)
+        for row in csv_data:
+            csv_writer.writerow(row)
+            e_csv_writer.writerow(row)
+
+    actual_s3_objects = sorted([o.key for o in s3.Bucket(bucket_name).objects.all()])
+    
+    # Test files written to s3
+    expected_s3_objects = [f"{folder}{filename}-0.{ext}", f"{folder}{filename}-1.{ext}"]
+    assert expected_s3_objects == actual_s3_objects
+
+    expected = expected_file.getvalue()
+    
+    # Test file contents
+    actual = ""
+    for expeceted_object in expected_s3_objects:
+        file_object = BytesIO()
+        s3.Object(bucket_name, expeceted_object).download_fileobj(file_object)
+        if compress:
+            actual += gzip.decompress(file_object.getvalue()).decode("utf-8")
+        else:
+            actual += file_object.getvalue().decode("utf-8")
+        file_object.close()
+
+    assert actual == expected
 
 
-MAX_BYTES = 80000
-CHUNK_SIZE = 1000
+@pytest.mark.parametrize(
+    "folder,filename,compress",
+    [
+        ("test-csv/", "test-file", False),
+        ("", "a", True),
+    ],
+)
+def test_csv_string_split_file_writer(s3, folder, filename, compress):
+    """Test string writer csv"""
+    bucket_name = "test"
+    s3.meta.client.create_bucket(Bucket=bucket_name)
+
+    csv_data = [("i","x1","x2"), (1,"a","b"), (2,"a","b"), (3,"a","b"),(4,"a","b"),(5,"a","b"),(6,"a","b"),(7,"a","b")]
+
+    expected_file = StringIO()
+    e_csv_writer = csv.writer(expected_file)
+
+    ext = "csv.gz" if compress else "csv"
+    
+    # Test using with statement
+    f = StringSplitFileWriter(f"s3://{bucket_name}/{folder}", filename, max_bytes=30, compress_on_upload=compress, file_extension=ext)
+    csv_writer = csv.writer(f)
+    for row in csv_data:
+        csv_writer.writerow(row)
+        e_csv_writer.writerow(row)
+    f.close()
+
+    actual_s3_objects = sorted([o.key for o in s3.Bucket(bucket_name).objects.all()])
+    
+    # Test files written to s3
+    expected_s3_objects = [f"{folder}{filename}-0.{ext}", f"{folder}{filename}-1.{ext}"]
+    assert expected_s3_objects == actual_s3_objects
+
+    
+    # Test file contents
+    expected = expected_file.getvalue()
+    actual = ""
+    for expeceted_object in expected_s3_objects:
+        file_object = BytesIO()
+        s3.Object(bucket_name, expeceted_object).download_fileobj(file_object)
+        if compress:
+            actual += gzip.decompress(file_object.getvalue()).decode("utf-8")
+        else:
+            actual += file_object.getvalue().decode("utf-8")
+        file_object.close()
+
+    assert actual == expected
 
 
-def write_with_writer(result_set):
-    with JsonNlSplitFileWriter(
-        "s3://test/test-file.josnl.gz", MAX_BYTES, CHUNK_SIZE
-    ) as writer:
-        writer.write_lines(result_set)
+@pytest.mark.parametrize(
+    "folder,filename,compress,filewriter_type",
+    [
+        ("test-jsonl/", "test-jsonl", False, "bytes"),
+        ("", "a", True, "bytes"),
+        ("test-jsonl/", "test-jsonl", False, "string"),
+        ("", "a", True, "string"),
+    ],
+)
+def test_split_file_writer_with_json(s3, folder, filename, compress, filewriter_type):
+    """Test jsonline string and bytes writer"""
+    
+    bucket_name = "test"
+    ext = "jsonl.gz" if compress else "jsonl"
 
+    s3.meta.client.create_bucket(Bucket=bucket_name)
 
-def write_manually(result_set):
-    string = ""
-    num_files = 0
-    num_lines = 0
-    while True:
-        for l in result_set:
-            string += f"{l}"
-            if not num_lines % CHUNK_SIZE and sys.getsizeof(string) > MAX_BYTES:
-                gzip_string_write_to_s3(
-                    string, f"s3://test/test-file-two_{num_files}.josnl.gz"
-                )
-                num_files += 1
-                num_lines = 0
-                string = ""
-            num_lines += 1
-        break
-    if string:
-        gzip_string_write_to_s3(string, f"s3://test/test-file-two_{num_files}.josnl.gz")
+    jsonl_data = [
+        {"i": 1, "x1": "a","x2": "b"},
+        {"i": 2, "x1": "a","x2": "b"},
+        {"i": 3, "x1": "a","x2": "b"},
+        {"i": 4, "x1": "a","x2": "b"},
+        {"i": 5, "x1": "a","x2": "b"},
+    ]
 
+    if filewriter_type == "string":
+        f = StringSplitFileWriter(f"s3://{bucket_name}/{folder}", filename, max_bytes=60, compress_on_upload=compress, file_extension=ext)
 
-def test_speed_of_writer(result_set, s3):
-    """
-    Test that generator is not much slower than a flat list
-    """
-    s3.meta.client.create_bucket(Bucket="test")
+    elif filewriter_type == "bytes":
+        f = BytesSplitFileWriter(f"s3://{bucket_name}/{folder}", filename, max_bytes=60, compress_on_upload=compress, file_extension=ext)
 
-    range_time = time_func(write_manually, result_set)
+    else:
+        raise ValueError("Input filewriter_type must be either 'string' or 'bytes'")
 
-    qs_time = time_func(write_with_writer, result_set)
+    
 
-    assert qs_time < range_time
+    # Write data
+    j_writer = jsonlines.Writer(f)
+    
+    expected_file = StringIO()
+    e_j_writer = jsonlines.Writer(expected_file)
+    
+    for row in jsonl_data:
+        j_writer.write(row)
+        e_j_writer.write(row)
+    f.close()
+
+    actual_s3_objects = sorted([o.key for o in s3.Bucket(bucket_name).objects.all()])
+    
+    # Test files written to s3
+    expected_s3_objects = [f"{folder}{filename}-0.{ext}", f"{folder}{filename}-1.{ext}", f"{folder}{filename}-2.{ext}"]
+    assert expected_s3_objects == actual_s3_objects
+    
+    # Test file contents
+    expected = expected_file.getvalue()
+    actual = ""
+    for expeceted_object in expected_s3_objects:
+        file_object = BytesIO()
+        s3.Object(bucket_name, expeceted_object).download_fileobj(file_object)
+        if compress:
+            actual += gzip.decompress(file_object.getvalue()).decode("utf-8")
+        else:
+            actual += file_object.getvalue().decode("utf-8")
+        file_object.close()
+
+    assert actual == expected
