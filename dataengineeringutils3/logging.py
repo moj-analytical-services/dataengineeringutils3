70fda7b47d70866351b862c6faed0b201c0c3028
s-ducks <stephen.bias@digital.justice.gov.uk>
2022-01-05 16:33:31 +0000

    added test and black format

--- dataengineeringutils3/logging.py
+++ dataengineeringutils3/logging.py
@@ -7,6 +7,7 @@ from typing import Tuple
 default_fmt = "%(asctime)s | %(funcName)s | %(levelname)s | %(context)s | %(message)s"
 default_date_fmt = "%Y-%m-%d %H:%M:%S"
 
+
 class ContextFilter(logging.Filter):
     """
     This is just overkill to apply a default context param to the log.
@@ -20,7 +21,9 @@ class ContextFilter(logging.Filter):
         return True
 
 
-def get_logger(fmt=default_fmt, datefmt=default_date_fmt) -> Tuple[logging.Logger, io.StringIO]:
+def get_logger(
+    fmt=default_fmt, datefmt=default_date_fmt
+) -> Tuple[logging.Logger, io.StringIO]:
     """
     returns a logger object and an io stream of the data that is logged
     """
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -23,8 +23,34 @@ def test_output(context_filter):
 
     # ensure it matches the required pattern
     regex = re.compile(
-        r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \| " 
+        r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \| "
         f"test_output \| INFO \| {context} \| {log_message}$"
+    )
+
+    assert regex.search(a)
+
+
+def test_diff_fmt():
+    """
+    makes sure different formats work correctly
+    """
+
+    # get the logger and the IO stream
+    logger, logger_io_stream = get_logger(
+        fmt="%(asctime)s | %(module)s %(table)s | %(levelname)s | %(message)s"
+    )
+
+    # log and retrieve a message
+    log_message = "a message!"
+    logger.info(log_message, extra={"table": "a_very_nice_table"})
+    a = logger_io_stream.getvalue()
+
+    print("hi")
+
+    # ensure it matches the required pattern
+    regex = re.compile(
+        r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \| "
+        f"{__name__.split('.')[1]} a_very_nice_table \| INFO \| {log_message}$"
     )
 
     assert regex.search(a)
