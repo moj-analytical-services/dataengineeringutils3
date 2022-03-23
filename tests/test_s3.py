585f5b747788607fea58e60a5cafe9e5285ebd3d
sbias <63045405+s-ducks@users.noreply.github.com>
2022-03-23 14:07:31 +0000

    Update test_s3.py

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -102,7 +102,7 @@ def test_read_json_from_s3(s3, bucket):
 
     assert read_json_from_s3("s3://test/f1/agfa/file_no_ext") == test_dict
 
-def test_read_yaml_from_s3(s3):
+def test_read_yaml_from_s3(s3, bucket):
 
     test_dict = {"foo": "bar"}
     body = yaml.dump(test_dict)
